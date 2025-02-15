63971658b269c8e1ae09365f72017b32e5237298
Nick Lian <36119434+Backyham@users.noreply.github.com>
2025-02-15 23:54:05 +0800

    Bugfix: NLTK Update - https://github.com/nltk/nltk/issues/3293

---
+++ ass_generator_OOPtest.py
@@ -0,0 +1,8 @@
+"""
+A OOP try-out, an attempt to gererate ASS subtitle directly from Whisper ASR json.
+WIP.
+"""
+
+class AegiLine:
+    def __init__(self):
+        pass
--- nltk_func.py
+++ nltk_func.py
@@ -5,7 +5,8 @@ def nltk_proc(text_result: str):
     try:
         sentences = tokenize.sent_tokenize(text_result)
     except LookupError:
-        download("punkt")
+        #download("punkt")
+        download("punkt_tab")
         sentences = tokenize.sent_tokenize(text_result)
     processed_text = '\n'.join(sentences)
     return processed_text
